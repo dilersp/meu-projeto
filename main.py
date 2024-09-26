69eba9e759c3332899fd65124517397319152677
Dilermando Piva Junior <dilers@gmail.com>
2024-09-26 11:52:55 -0300

    Alterações do arquivo main.py por DILERSP

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("ESte é um arquivo inicial do meu-projeto")
 print("Essa modificação foi efetuada pelo colaborador pyprobr")
+if True:
+    print("Mostre essa mensagem")
+else:
+    print("Mostre essa outra mensagem")
